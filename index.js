213fef15bee015980f827b2e352f8b920f96b35c
Yan Emanuel <yanemanuel20@outlook.com>
2022-10-11 21:23:22 -0300

    segund commit

---
+++ index.js
@@ -0,0 +1,46 @@
+function saboresB(){
+    var troca = document.getElementById('sabor');
+    var trocaNome = document.getElementById('trocaDeNome');
+
+    troca.setAttribute('src', './img/baunilha1.png');
+    troca.setAttribute('style', 'width: 800px;');
+
+    trocaNome.innerHTML = "100% Whey Refil 900G - Max Titanium - Baunilha";
+    trocaNome.setAttribute('style', 'color: #e4cc94;')
+}
+
+
+function saboresC(){
+    var troca = document.getElementById('sabor');
+    var trocaNome = document.getElementById('trocaDeNome');
+
+    troca.setAttribute('src', './img/chocolate1.png');
+    troca.setAttribute('style', 'width: 800px;')
+
+    trocaNome.innerHTML = "100% Whey Refil 900G - Max Titanium - Chocolate";
+    trocaNome.setAttribute('style', 'color: #361e15;')
+}
+
+
+function saboresM(){
+    var troca = document.getElementById('sabor');
+    var trocaNome = document.getElementById('trocaDeNome');
+
+    troca.setAttribute('src', './img/morango1.png');
+    troca.setAttribute('style', 'width: 800px;')
+
+    trocaNome.innerHTML = "100% Whey Refil 900G - Max Titanium - Morango";
+    trocaNome.setAttribute('style', 'color: #b9001c;')
+}
+
+
+function saboresCO(){
+    var troca = document.getElementById('sabor');
+    var trocaNome = document.getElementById('trocaDeNome');
+
+    troca.setAttribute('src', './img/whey.png');
+    troca.setAttribute('style', 'width: 800px;');
+
+    trocaNome.innerHTML = "100% Whey Refil 900G - Max Titanium - Cookies";
+    trocaNome.setAttribute('style', 'color: #8b7960;')
+}
